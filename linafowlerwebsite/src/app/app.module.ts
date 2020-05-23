e7bea4fd0fb568a54cde70b77caabf0209be4685
Lina Langford <lina.langford235@gmail.com>
2020-05-23 11:35:47 -0600

    added background to landing and navbar

--- linafowlerwebsite/src/app/app.module.ts
+++ linafowlerwebsite/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    PortfolioComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
