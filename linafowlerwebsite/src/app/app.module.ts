7e00980f99365bae8e544c90b81f4b68943cf87c
Lina Langford <lina.langford235@gmail.com>
2020-06-02 18:07:04 -0600

    Added services and animations

--- linafowlerwebsite/src/app/app.module.ts
+++ linafowlerwebsite/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { AboutComponent } from './components/about/about.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ServicesComponent } from './components/services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     PortfolioComponent,
-    LandingComponent
+    LandingComponent,
+    AboutComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ linafowlerwebsite/src/app/components/services/services.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+
+@Component({
+  selector: 'app-services',
+  templateUrl: './services.component.html',
+  styleUrls: ['./services.component.scss']
+})
+export class ServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    AOS.init({
+      duration: 1200,
+    })
+  }
+
+}
